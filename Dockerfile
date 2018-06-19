93a763adce2e26b1b2ca1082bcb9c3ff038345cb
Ville Nupponen <aikain@gosu.fi>
2018-06-19 06:22:53 +0200

    Initiliaze commit: Add first version (Dockerfile from nginz:alpine, install php7, install wp, set wp-config from envs, install plugins and themes from envs)

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+FROM nginx:alpine
+
+LABEL maintainer="Dalitso / Ville Nupponen <docker@dalitso.fi>"
+
+ENV WP_VERSION 4.9.6
+
+RUN apk update \
+	&& apk add bash php7 php7-fpm php7-mysqli
+
+RUN addgroup -S www-data \
+	&& adduser -SDh /var/www www-data www-data \
+	&& mkdir -p /var/www/wp \
+	&& wget https://wordpress.org/wordpress-$WP_VERSION.tar.gz -O wp.tar.gz \
+	&& tar -C /var/www/wp -xf wp.tar.gz --strip 1 \
+	&& rm wp.tar.gz \
+        && rm -rf /var/www/wp/wp-content \
+	&& chown -R www-data:www-data /var/www/wp
+
+ADD ./nginx-default.conf /etc/nginx/conf.d/default.conf
+RUN sed -i -e 's/nobody/www-data/g' /etc/php7/php-fpm.d/www.conf
+
+COPY ./docker-entrypoint.sh /
+RUN chmod +x /docker-entrypoint.sh
+ENTRYPOINT ["/docker-entrypoint.sh"]
+
+VOLUME /var/www/wp/wp-content
+EXPOSE 80
+
+CMD ["nginx", "-g", "daemon off;"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+set -e
+
+# From https://wordpress.org/themes/
+themes=()
+if [[ ! -z "${WORDPRESS_THEMES}" ]]; then
+    IFS=',' read -r -a themes <<< "${WORDPRESS_THEMES}"
+fi
+themes+=(twentyseventeen)
+
+# From https://wordpress.org/plugins/
+plugins=()
+if [[ ! -z "${WORDPRESS_PLUGINS}" ]]; then
+    IFS=',' read -r -a plugins <<< "${WORDPRESS_PLUGINS}"
+fi
+
+echo ""
+echo "Install themes.."
+if [ ! -d "/var/www/wp/wp-content/themes/$i" ]; then
+    mkdir -p /var/www/wp/wp-content/themes/
+fi
+for i in "${themes[@]}"; do
+    if [ ! -d "/var/www/wp/wp-content/themes/$i" ]; then
+        echo -n "> $i.. "
+        wget -q "https://downloads.wordpress.org/theme/$i.zip" -O theme.zip
+        unzip -q theme.zip -d /var/www/wp/wp-content/themes/
+        rm theme.zip
+        echo "OK"
+    else
+        echo "> $i.. (already installed)"
+    fi
+done
+echo "Themes installed"
+echo ""
+echo "Install plugins.."
+if [ ! -d "/var/www/wp/wp-content/plugins/$i" ]; then
+    mkdir -p /var/www/wp/wp-content/plugins/
+fi
+for i in "${plugins[@]}"; do
+    if [ ! -d "/var/www/wp/wp-content/plugins/$i" ]; then
+        echo -n "> $i.. "
+        wget -q "https://downloads.wordpress.org/plugin/$i.zip" -O plugin.zip
+        unzip -q plugin.zip -d /var/www/wp/wp-content/plugins/
+        rm plugin.zip
+        echo "OK"
+    else
+        echo "> $i.. (already installed)"
+    fi
+done
+echo "Plugins installed"
+echo ""
+
+chown -R www-data:www-data /var/www/wp/wp-content/
+
+echo -n "Configuring.. "
+DB_NAME=$(if [[ ! -z "${DB_NAME}" ]]; then echo "${DB_NAME}"; else echo "wordpress"; fi)
+DB_USER=$(if [[ ! -z "${DB_USER}" ]]; then echo "${DB_USER}"; else echo "root"; fi)
+DB_PASSWORD=$(if [[ ! -z "${DB_PASSWORD}" ]]; then echo "${DB_PASSWORD}"; else echo "root-password"; fi)
+DB_HOST=$(if [[ ! -z "${DB_HOST}" ]]; then echo "${DB_HOST}"; else echo "db"; fi)
+DB_TABLE_PREFIX=$(if [[ ! -z "${DB_TABLE_PREFIX}" ]]; then echo "${DB_TABLE_PREFIX}"; else echo "wp_"; fi)
+WP_DEBUG=$(if [[ ! -z "${WP_DEBUG}" && "${WP_DEBUG}" = "true" ]]; then echo true; else echo false; fi)
+
+cat <<EOF > /var/www/wp/wp-config.php
+<?php
+
+define('DB_NAME', "$DB_NAME");
+define('DB_USER', "$DB_USER");
+define('DB_PASSWORD', "$DB_PASSWORD");
+define('DB_HOST', "$DB_HOST");
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
+
+$(wget -q https://api.wordpress.org/secret-key/1.1/salt/ -O -)
+
+\$table_prefix  = "$DB_TABLE_PREFIX";
+
+define('WP_DEBUG', $WP_DEBUG);
+
+define('FORCE_SSL_ADMIN', true);
+if (strpos(\$_SERVER['HTTP_X_FORWARDED_PROTO'], 'https') !== false)
+        \$_SERVER['HTTPS']='on';
+
+if ( !defined('ABSPATH') )
+        define('ABSPATH', dirname(__FILE__) . '/');
+
+require_once(ABSPATH . 'wp-settings.php');
+EOF
+
+echo "OK"
+
+
+/usr/sbin/php-fpm7
+
+exec "$@"
